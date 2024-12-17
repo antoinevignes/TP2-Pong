25a62372f39ef4ff40b9276330049c438d1ac384
Antoine Vignes <168306302+antoinevignes@users.noreply.github.com>
2024-12-17 14:52:19 +0100

    Mouvement de la balle

--- js/main.js
+++ js/main.js
@@ -5,4 +5,13 @@ const context = canvas.getContext("2d");
 
 const ball = new Ball(320, 180, 10, "red");
 
-ball.draw(context);
+function gameLoop() {
+  context.clearRect(0, 0, canvas.clientWidth, canvas.height);
+
+  ball.move(context);
+  ball.draw(context);
+
+  requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
